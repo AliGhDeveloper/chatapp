fc2f885e74086f6deb8c8be5f3f1976af9a82f9b
Ali gh <ghasempour.2m@gmail.com>
2022-12-13 13:06:34 +0330

    fixin build probs

--- pages/_document.js
+++ pages/_document.js
@@ -9,7 +9,7 @@ export default class document extends Document {
                     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css" />
                     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" async  crossOrigin="anonymous" />
                     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" async crossOrigin="anonymous"></script>
-                    <script src="https://js.pusher.com/5.0/pusher.min.js"></script>
+                    <script src="https://js.pusher.com/5.0/pusher.min.js" async></script>
                 </Head>
                 <body>
                     <Main />
